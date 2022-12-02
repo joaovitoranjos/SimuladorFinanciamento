b28b59e03c38cf1b8b20140a5a49ba62440fc1e6
joaoalmeids00@gmail.com <joaoalmeids00@gmail.com>
2022-12-02 18:29:18 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const comCarencia = document.querySelector('#comCarencia');
+const listaSuspensa = document.querySelector('#listaSuspensa');
+const corpoTabela = document.querySelector('#corpoTabela')
+
+comCarencia.addEventListener('change',function() {
+    if(this.checked){
+        listaSuspensa.removeAttribute('hidden');
+    } else {
+        listaSuspensa.setAttribute('hidden','hidden');
+    }
+});
